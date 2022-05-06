65c19e075aed895d1e84146d1df55a7fdb167e09
Sam Komarov <sam@skomarov.com>
2022-05-06 16:55:35 +0300

    tmp

--- instagram_app/views/profiles_view_set.py
+++ instagram_app/views/profiles_view_set.py
@@ -43,7 +43,7 @@ class ProfilesViewSet(ViewSet):
             profile.save() 
         if request.data.get('avatar'): 
             image_file = request.data['avatar'] 
-            # print(type(image_file))
+            print(type(image_file))
             # if not issubclass(type(image_file), UploadedFile):
             #     return Response(status=400)
             # check that provided file is truly a valid image (not some .js file or something like that)
