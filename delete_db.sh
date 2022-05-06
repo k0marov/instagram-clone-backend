1a92921a2bda902c68405eeb8a4572b62138cacf
Sam Komarov <sam@skomarov.com>
2022-05-06 15:39:58 +0300

    fix delete_db.sh

--- delete_db.sh
+++ delete_db.sh
@@ -3,4 +3,4 @@ find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 find . -path "*.sqlite3"  -delete
 ./manage.py makemigrations
-./manage.py migrate --run-sync-db
+./manage.py migrate --run-syncdb
